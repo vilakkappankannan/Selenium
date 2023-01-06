338f1f81c3a9cb4dccd73fea946b81c407721521
Vilakkappan Kannan <VilakkappanKannan@discover.com>
2023-01-06 16:20:19 +0000

    Updated step definition

--- src/main/java/steps/StepDefinition.java
+++ src/main/java/steps/StepDefinition.java
@@ -6,7 +6,13 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class StepDefinition {
@@ -26,6 +32,23 @@ public class StepDefinition {
         driver.findElement(By.id("user_login")).sendKeys("");
         driver.findElement(By.id("passwd_login")).sendKeys("");
         driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[5]/button")).click();
+        driver.findElement(By.xpath("//*[@id=\"redesign-beta-optin-btn\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"AppRouter-main-content\"]/div/div/div[2]/div[2]/div[1]/div[1]/input")).click();
+
+//        Select community = new Select(driver.findElement(By.xpath("//*[@id="AppRouter-main-content"]/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/i")));
+//        community.selectByValue("u/vilakkappan");
+
+        Duration timeoutInSeconds = null;
+        WebDriverWait wait = new WebDriverWait(driver, null);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"AppRouter-main-content\"]/div/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/i")));
+
+//
+        driver.findElement(By.xpath("//*[@id=\"AppRouter-main-content\"]/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div[2]/div[1]/div/textarea")).sendKeys("Testing");
+//        driver.findElement(By.name("button")).click();
+
+
+
+
     }
 
     @Then("login successful")
--- src/main/resources/features/selenium/Test.feature
+++ src/main/resources/features/selenium/Test.feature
@@ -1,7 +1,13 @@
 Feature: Test
 
-  @smoke
-  Scenario: Login Test
+#  @smoke
+#  Scenario: Login Test
+#    Given login to mail
+#    When enter the username and password
+#    Then login successful
+
+  @smoke @vk
+  Scenario: create post
     Given login to mail
     When enter the username and password
     Then login successful
