7f2c88f2223fc460d0e1ebd2e4372f8f55419b3c
Vilakkappan Kannan <VilakkappanKannan@discover.com>
2023-02-06 15:18:29 +0000

    Added gatling performance

---
+++ src/main/java/gatling/demo.java
@@ -0,0 +1,24 @@
+package gatling;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class demo extends Simulation {
+
+    HttpProtocolBuilder req = http.baseUrl("https://example.com");
+
+    ScenarioBuilder scenario = scenario("HelloWorld")
+            .exec(http("TOO_Home")
+            .get("/")
+            .check(
+                   status().is(201),
+                   status().is(500)
+            ))
+            .pause(3);
+    {
+        setUp(scenario.injectOpen(atOnceUsers(1)).protocols(req));
+    }
+}
