8bc61e12459ee819a436e61acffb6be64e1fa830
Vilakkappan Kannan <VilakkappanKannan@discover.com>
2023-02-14 11:21:10 +0000

    updated gatling

--- src/gatling/java/demo/BasicSimulation.java
+++ src/gatling/java/demo/BasicSimulation.java
@@ -1,29 +1,39 @@
 package demo;
 
+import io.gatling.core.body.RawFileBody;
+import io.gatling.core.body.StringBody;
+import io.gatling.javaapi.core.CoreDsl;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpDsl;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
-import static io.gatling.javaapi.http.HttpDsl.http;
-import static io.gatling.javaapi.http.HttpDsl.status;
+import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class BasicSimulation extends Simulation {
     HttpProtocolBuilder req = http.baseUrl("http://localhost:8082/eligibility/test");
 
     ScenarioBuilder scenario = scenario("HelloWorld")
-            .exec(http("TOO_Home")
-                    .post("/")
+            .exec(http("Eligibility")
+                    .post("")
                     .header("accept", "application/json")
                     .header("content-type","application/json")
                     .header("cache-control", "no-store")
-                    .check(
-                            status().is(200),
-                            status().is(500)
-                    ))
+                    .asJson()
+                            .body(CoreDsl.ElFileBody("json/request/EligibilityRequest.json"))
+                    .check(HttpDsl.status().is(200))
+                            .check(CoreDsl.responseTimeInMillis().lte(3000))
+                    )
             .pause(3);
+
     {
-        setUp(scenario.injectOpen(atOnceUsers(1)).protocols(req));
+        setUp(scenario.injectOpen(
+                CoreDsl.nothingFor(5),
+                CoreDsl.atOnceUsers(1),
+                CoreDsl.rampUsers(2).during(10))
+
+                .protocols(req));
     }
 }
