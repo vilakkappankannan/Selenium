89f684e50289a6e02bf5bb241f951d93dc3ebc9e
Vilakkappan Kannan <VilakkappanKannan@discover.com>
2023-02-14 12:04:37 +0000

    updated gatling

--- src/gatling/java/demo/BasicSimulation.java
+++ src/gatling/java/demo/BasicSimulation.java
@@ -1,26 +1,30 @@
 package demo;
 
-import io.gatling.core.body.RawFileBody;
-import io.gatling.core.body.StringBody;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import io.gatling.javaapi.core.CoreDsl;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpDsl;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class BasicSimulation extends Simulation {
-    HttpProtocolBuilder req = http.baseUrl("http://localhost:8082/eligibility/test");
+//    HttpProtocolBuilder req = http.baseUrl("http://localhost:8082/eligibility/test");
 
-    ScenarioBuilder scenario = scenario("HelloWorld")
-            .exec(http("Eligibility")
+    Config config = ConfigFactory.load("performance.properties");
+    String baseUrl = config.getString("BASE_URL");
+
+    HttpProtocolBuilder req = http.baseUrl(baseUrl);
+
+            ScenarioBuilder scenario = CoreDsl.scenario(config.getString("SCENARIO_NAME"))
+            .exec(HttpDsl.http("Eligibility API")
                     .post("")
-                    .header("accept", "application/json")
-                    .header("content-type","application/json")
-                    .header("cache-control", "no-store")
+                    .header("Accept", config.getString("ACCEPT"))
+                    .header("Content-Type", config.getString("CONTENT_TYPE"))
+                    .header("Cache-Control", config.getString("CACHE_CONTROL"))
                     .asJson()
                             .body(CoreDsl.ElFileBody("json/request/EligibilityRequest.json"))
                     .check(HttpDsl.status().is(200))
@@ -32,8 +36,14 @@ public class BasicSimulation extends Simulation {
         setUp(scenario.injectOpen(
                 CoreDsl.nothingFor(5),
                 CoreDsl.atOnceUsers(1),
-                CoreDsl.rampUsers(2).during(10))
+                CoreDsl.rampUsers(2).during(10)
+                )
+                .protocols(req))
 
-                .protocols(req));
+        //assertion
+            .assertions(CoreDsl.global().responseTime().percentile3().lte(100))    // for all requests - percentile3 => 95 - ms
+            .assertions(CoreDsl.forAll().responseTime().max().lte(100))            // stats for individual requests - max response time in ms
+            .assertions(CoreDsl.forAll().failedRequests().percent().lte(0.01));       // for all failed requests percentage < 0.01%
+
     }
 }
